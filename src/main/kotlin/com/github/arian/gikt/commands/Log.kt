05fa39feb4edb31b9a9bc59cf7a4a7b1415d4de6
Arian <stolwijk.arian@gmail.com>
2020-10-10 19:49:41 +0200

    16.1 git log onelin format and abbreviate commit hash options

--- src/main/kotlin/com/github/arian/gikt/commands/Log.kt
+++ src/main/kotlin/com/github/arian/gikt/commands/Log.kt
@@ -4,13 +4,22 @@ import com.github.arian.gikt.commands.util.Style
 import com.github.arian.gikt.database.Commit
 import com.github.arian.gikt.database.ObjectId
 import com.github.arian.gikt.utf8
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
 
 class Log(ctx: CommandContext, name: String) : AbstractCommand(ctx, name) {
+
+    private enum class Format {
+        MEDIUM,
+        ONELINE
+    }
+
+    private val options = Options()
 
     override fun run() {
         eachCommit()
             .takeWhile { shouldContinuePrinting() }
-            .forEachIndexed { index, commit -> showCommit(commit, index) }
+            .forEachIndexed { index, commit -> println(showCommit(commit, index)) }
     }
 
     private fun eachCommit(): Sequence<Commit> {
@@ -21,21 +30,77 @@ class Log(ctx: CommandContext, name: String) : AbstractCommand(ctx, name) {
     private fun loadCommit(oid: ObjectId?): Commit? =
         oid?.let { repository.loadObject(it) as? Commit }
 
-    private fun showCommit(commit: Commit, index: Int) {
-        if (index != 0) {
-            println("")
+    private fun showCommit(commit: Commit, index: Int): String {
+        return when (options.formatOption) {
+            Format.MEDIUM -> formatMedium(commit, index)
+            Format.ONELINE -> formatOneline(commit)
         }
+    }
 
+    private fun formatMedium(commit: Commit, index: Int): String {
         val author = commit.author
 
-        println(fmt(Style.YELLOW, "commit ${commit.oid}"))
-        println(
+        val space = if (index != 0) {
+            "\n"
+        } else {
+            ""
+        }
+
+        val log =
             """
+            |${fmt(Style.YELLOW, "commit ${abbrev(commit.oid)}")}
             |Author: ${author.name} <${author.email}>
             |Date:   ${author.readableTime}
             |
             |${commit.message.utf8().trimEnd().prependIndent()}
             """.trimMargin()
+
+        return space + log
+    }
+
+    private fun formatOneline(commit: Commit): String {
+        return "${fmt(Style.YELLOW, abbrev(commit.oid))} ${commit.title}"
+    }
+
+    private fun abbrev(oid: ObjectId): String {
+        return if (options.isAbbrevCommitOption) {
+            oid.short
+        } else {
+            oid.hex
+        }
+    }
+
+    private inner class Options {
+        private val abbrevCommit by option(
+            ArgType.Boolean,
+            fullName = "abbrev-commit",
+            description = "Instead of showing the full 40-byte hexadecimal commit object name, " +
+                "show only a partial prefix."
         )
+
+        private val noAbbrevCommit by option(
+            ArgType.Boolean,
+            fullName = "no-abbrev-commit",
+            description = "Show the full 40-byte hexadecimal commit object name."
+        )
+
+        private val format by option(
+            ArgType.Choice<Format>(),
+            description = "Pretty-print the contents of the commit logs in a given format."
+        ).default(Format.MEDIUM)
+
+        private val oneline by option(
+            ArgType.Boolean,
+            description = "This is a shorthand for \"--format=oneline --abbrev-commit\" used together"
+        )
+
+        val isAbbrevCommitOption: Boolean get() =
+            ((abbrevCommit == true || oneline == true) && noAbbrevCommit != false)
+
+        val formatOption: Format get() =
+            when (oneline) {
+                true -> Format.ONELINE
+                else -> format
+            }
     }
 }
--- src/test/kotlin/com/github/arian/gikt/commands/LogTest.kt
+++ src/test/kotlin/com/github/arian/gikt/commands/LogTest.kt
@@ -41,4 +41,57 @@ internal class LogTest {
             execution.stdout
         )
     }
+
+    @Test
+    fun `abbrev-commit option should abbreviate the commit hash`() {
+        cmd.commitFile("a", msg = "first")
+        val execution = cmd.cmd("log", "--abbrev-commit")
+        assertEquals(0, execution.status)
+        assertFalse(execution.stdout.startsWith("\n"))
+        assertEquals(
+            """
+            |commit c437776
+            |Author: Arian <arian@example.com>
+            |Date:   Wed Aug 14 12:08:22 2019 +0200
+            |
+            |    first
+            |
+            """.trimMargin(),
+            execution.stdout
+        )
+    }
+
+    @Test
+    fun `format=oneline should show each commit on a single line`() {
+        val first = cmd.commitFile("a", msg = "first")
+        val second = cmd.commitFile("a", msg = "second\nfoo\nbar")
+        val execution = cmd.cmd("log", "--format", "oneline")
+        assertEquals(0, execution.status)
+        assertFalse(execution.stdout.startsWith("\n"))
+        assertEquals(
+            """
+            |$second second
+            |$first first
+            |
+            """.trimMargin(),
+            execution.stdout
+        )
+    }
+
+    @Test
+    fun `oneline option should show each commit abbreviated on a single line`() {
+        cmd.commitFile("a", msg = "first")
+        cmd.commitFile("a", msg = "second\nfoo\nbar")
+        val execution = cmd.cmd("log", "--oneline")
+        assertEquals(0, execution.status)
+        assertFalse(execution.stdout.startsWith("\n"))
+        assertEquals(
+            """
+            |b195aa1 second
+            |c437776 first
+            |
+            """.trimMargin(),
+            execution.stdout
+        )
+    }
 }
